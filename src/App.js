6218331b355ac951f0181f0901c4f48f060bfd68
Strong10mede <kmayur809@gmail.com>
2022-12-29 10:11:09 +0530

    app.js router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import "./App.css";
-
+import { Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Orders from "./Components/Orders/Orders";
+import Login from "./Components/Login/Login";
+import Home from "./Components/Home/Home";
+import Checkout from "./Components/Checkout/Checkout";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route
+            path="/orders"
+            element={
+              <>
+                <Header />
+                <Orders />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
