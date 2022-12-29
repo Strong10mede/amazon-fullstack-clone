1617212c3a7b1564598e8744ef70fd814628d33c
Strong10mede <kmayur809@gmail.com>
2022-12-29 11:36:20 +0530

    header partial done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/login">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">SignIn</span>
+          </div>
+        </Link>
+        <Link to="/login">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">SignIn</span>
+          </div>
+        </Link>
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              3
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
