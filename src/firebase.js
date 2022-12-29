76d4cdf7cce4acb4e34f431230da5772dcc44436
Strong10mede <kmayur809@gmail.com>
2022-12-29 11:07:16 +0530

    firebase done

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +12,14 @@ const firebaseConfig = {
   projectId: "clone-688fc",
   storageBucket: "clone-688fc.appspot.com",
   messagingSenderId: "331945311138",
-  appId: "1:331945311138:web:163edfe0bad51f938810fb"
+  appId: "1:331945311138:web:163edfe0bad51f938810fb",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider, signInWithPopup };
