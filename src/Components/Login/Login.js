b32c26a3c6a53701e432ea88a75377640ea6ccb2
Strong10mede <kmayur809@gmail.com>
2022-12-30 14:37:46 +0530

    firebase done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,57 @@
 import React from "react";
 import "./Login.css";
+import { auth, provider, signInWithPopup } from "../../firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = () => {};
+  const register = () => {};
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          src="src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+          className="login__logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
