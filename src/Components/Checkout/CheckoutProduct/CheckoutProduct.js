84dd1a72d3b1ac55c466880377ab39bf5fada066
Strong10mede <kmayur809@gmail.com>
2022-12-30 15:21:23 +0530

    checkoutProduct done

--- src/Components/Checkout/CheckoutProduct/CheckoutProduct.js
+++ src/Components/Checkout/CheckoutProduct/CheckoutProduct.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./CheckoutProduct.css";
-function CheckoutProduct() {
-  return <div>CheckoutProduct</div>;
+import { useStateValue } from "../../../StateProvider";
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    //remove the item from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+  return (
+    <div className="checkoutProduct">
+      <img src={image} alt="" className="checkoutProduct__image" />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from Basket</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutProduct;
