426c5a853c4e322d8b3ec1ccca4137b70d20a284
Strong10mede <kmayur809@gmail.com>
2022-12-31 11:21:34 +0530

    onAuthStateChanged

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ import Orders from "./Components/Orders/Orders";
 import Login from "./Components/Login/Login";
 import Home from "./Components/Home/Home";
 import Checkout from "./Components/Checkout/Checkout";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 function App() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      console.log("The USER is >>>", authUser);
+
+      if (authUser) {
+        //the user wa logged in / the user was logged in
+
+        dispatchEvent({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user logged out
+        dispatchEvent({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <div className="app">
       <Router>
