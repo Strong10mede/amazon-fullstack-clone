188aad441505d3d5779ef0991499f2d38edfc63a
Strong10mede <kmayur809@gmail.com>
2022-12-31 20:22:26 +0530

    index.js partial done

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const stripe = require("stripe")(
+  "pk_test_51MKygaSJsvX4oW7TVNDXo4Qkcfhygqg" +
+    "CmcOmXkKGQk6YU5pcrUDDuHcaKMHa2Nnx3QINT5A" +
+    "MJKACz8ZYAyLMewzG00WG2DC0Fu"
+);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
